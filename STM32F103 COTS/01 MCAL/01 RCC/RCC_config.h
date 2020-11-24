9e69f7dbcc591710027da534b0934071d7455cea
Mohammed Abdelalim <MohamedLemo14@gmail.com>
2020-11-24 10:21:57 +0200

    Adding RCC driver files

--- STM32F103 COTS/01 MCAL/01 RCC/RCC_config.h
+++ STM32F103 COTS/01 MCAL/01 RCC/RCC_config.h
@@ -1,37 +1,79 @@
-/********************************************************************************************
-* @Author: Mohammed Abdelalim
-* @Version:
-* @Date: 10-11-2020
-*********************************************************************************************/
-
+/******************************************************************************
+* @file RCC_config.h
+* @brief This files contains the required RCC driver user configurations
+* @author: Mohammed Abdelalim
+* @version: V01
+* @date: 10-11-2020
+******************************************************************************/
 #ifndef RCC_CONFIG_H
 #define RCC_CONFIG_H
 
-/* @options : 	RCC_HSI
-*		RCC_HSE
-*		RCC_PLL >>>> ha7otohom f el private.h */
+																/* For HSI configurations */
+// #define RCC_CLOCKTYPE		RCC_HSI
 
-//#define RCC_CLOCKTYPE		RCC_HSI
-//#define RCC_CLOCKTYPE		RCC_HSE
-#define RCC_CLOCKTYPE		RCC_PLL
+																/* For HSE configurations */
+#define RCC_CLOCKTYPE		RCC_HSE
 
-/* Note: Select one of these values only if you select the PLL clock type */
-/* @options : 	RCC_PLL_HSE_DIV_2
-*		RCC_PLL_HSE
-*		RCC_PLL_HSI_DIV_2 */
+																/* For PLL configurations */
+//#define RCC_CLOCKTYPE		RCC_PLL
+/* Note: Select one of these macros only after you select the PLL clock type */
 #if RCC_CLOCKTYPE == RCC_PLL //for protection from el user el3'aby
-#define RCC_PLL_INPUT 	RCC_PLL_HSE
+// #define RCC_PLL_HSE_DIV_2
+// #define RCC_PLL_HSE
+// #define RCC_PLL_HSI_DIV_2
 #endif
 
-
-/* Note: Select one of these values only if you select the PLL clock type */
+/* Note: Select one of these macros only after you select the PLL clock type */
 /* @options : 	may be 2,3,4,5,6,7,...,16 */
 #if RCC_CLOCK_TYPE == RCC_PLL
-#define RCC_PLL_MUL_VAL		4
+// #define RCC_PLL_MUL_2
+// #define RCC_PLL_MUL_3
+// #define RCC_PLL_MUL_4
+// #define RCC_PLL_MUL_5
+// #define RCC_PLL_MUL_6
+// #define RCC_PLL_MUL_7
+// #define RCC_PLL_MUL_8
+// #define RCC_PLL_MUL_9
+// #define RCC_PLL_MUL_10
+// #define RCC_PLL_MUL_11
+// #define RCC_PLL_MUL_12
+// #define RCC_PLL_MUL_13
+// #define RCC_PLL_MUL_14
+// #define RCC_PLL_MUL_15
+// #define RCC_PLL_MUL_16
 #endif
 
+																/* For clock security system enable */
+																/* optional */
+// #define RCC_CSS
 
+																/* APB2 prescaling configurations */
+																/* mandatory */
+#define RCC_APB2_NO_PRESCALER
+// #define RCC_APB2_PRESCALER_2
+// #define RCC_APB2_PRESCALER_4
+// #define RCC_APB2_PRESCALER_8
+// #define RCC_APB2_PRESCALER_16
 
+																/* APB1 prescaling configurations */
+																/* mandatory */
+#define RCC_APB1_NO_PRESCALER
+// #define RCC_APB1_PRESCALER_2
+// #define RCC_APB1_PRESCALER_4
+// #define RCC_APB1_PRESCALER_8
+// #define RCC_APB1_PRESCALER_16
+
+																/* AHB prescaling configurations */
+																/* mandatory */
+#define RCC_AHB_NO_PRESCALER
+// #define RCC_AHB_PRESCALER_2
+// #define RCC_AHB_PRESCALER_4
+// #define RCC_AHB_PRESCALER_8
+// #define RCC_AHB_PRESCALER_16
+// #define RCC_AHB_PRESCALER_64
+// #define RCC_AHB_PRESCALER_128
+// #define RCC_AHB_PRESCALER_256
+// #define RCC_AHB_PRESCALER_512
 
 	
 
--- STM32F103 COTS/01 MCAL/01 RCC/RCC_program.c
+++ STM32F103 COTS/01 MCAL/01 RCC/RCC_program.c
@@ -6,12 +6,12 @@
 * @date: 10-11-2020
 *********************************************************************************************/
 
-#include "STD_TYPES.h"
+#include "../include/STD_TYPES.h"
 #include "BIT_MATH.h"
 
 // e3ml include b eltarteeb da (brattebhom 3ala 7sb el dependancy bta3t-hom)
-#include "RCC_interface.h" // lazm yb2a awl include 3shan el functions declarations bykoon feeha el macros elly f el 2 files elgayeen
-#include "RCC_private.h"
+#include "../include/RCC_interface.h" // lazm yb2a awl include 3shan el functions declarations bykoon feeha el macros elly f el 2 files elgayeen
+#include "../include/RCC_private.h"
 #include "RCC_config.h"
 
 void RCC_voidInit(void){
@@ -19,183 +19,193 @@ void RCC_voidInit(void){
 	// enable and select the required clock source to be the system clock
 	#if RCC_CLOCKTYPE == RCC_HSE
 	// enable clock source
-	RCC_CR |= (1<<16);
+	RCC_CR |= RCC_CR_HSE_ON;
 	// wait for the HSE ready flag to be set
-	while ((RCC_CR & (1<<17)) == 0);
+	while ((RCC_CR & RCC_CR_HSE_ON_RDY) == 0);
 	// select HSE as the system clock
-	RCC_CFGR |= (1<<0);
-	RCC_CFGR &= ~(1<<1);
-	//wait for the SWS flag to be set
-	while ((RCC_CFGR & (1<<0)) != 1);
+	RCC_CFGR |=  RCC_CFGR_SW_HSE1;
+	RCC_CFGR &= ~RCC_CFGR_SW_HSE2;
+	// wait for the SWS flag to be set
+	while ((RCC_CFGR & RCC_CFGR_SWS_HSE) != 4);
 
 	#elif RCC_CLOCKTYPE == RCC_HSI
-	RCC_CR |= (1<<0);
-	while ((RCC_CR & (1<<1)) == 0);
-	RCC_CFGR &= ~0x00000003;
-	while ((RCC_CFGR & 0x00000003) != 0);
+	// enable clock source
+	RCC_CR |= RCC_CR_HSI_ON;
+	// wait for the HSI ready flag to be set
+	while ((RCC_CR & RCC_CR_HSI_ON_RDY) == 0);
+	// select HSI as the system clock
+	RCC_CFGR &= ~RCC_CFGR_SW_HSI;
+	// wait for the SWS flag to be set
+	while ((RCC_CFGR & RCC_CFGR_SWS_HSI) != 0);
 
 	#elif RCC_CLOCKTYPE == RCC_PLL
 	// configure the PLL input clock source
-	#ifdef RCC_PLL_HSI_HALVED
-	RCC_CFGR &= ~(1<<16);
+	#ifdef RCC_PLL_HSI_DIV_2
+	// set the PLL entry clock source to HSI/2
+	RCC_CFGR &= ~RCC_CFGR_PLL_SRC;
 	#endif
 	#ifdef RCC_PLL_HSE
-	RCC_CFGR |= (1<<16);
-	RCC_CFGR &= ~(1<<17);
+	// set the PLL entry clock source to HSE
+	RCC_CFGR |= RCC_CFGR_PLL_SRC;
+	// disable the HSE divider for PLL entry
+	RCC_CFGR &= ~RCC_CFGR_PLL_XTPRE;
 	#endif
-	#ifdef RCC_PLL_HSE_HALVED
-	RCC_CFGR |= (1<<16);
-	RCC_CFGR |= (1<<17);
+	#ifdef RCC_PLL_HSE_DIV_2
+	// set the PLL entry clock source to HSE
+	RCC_CFGR |= RCC_CFGR_PLL_SRC;
+	// enable the HSE divider for PLL entry
+	RCC_CFGR |= RCC_CFGR_PLL_XTPRE;
 	#endif
 	// configure the PLL multiplication factor
 	// either x(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
 	#ifdef RCC_PLL_MUL_2
-	RCC_CFGR &= ~0x003C0000;
+	RCC_CFGR &= ~RCC_PLLMUL_X2;
 	#endif
 	#ifdef RCC_PLL_MUL_3
-	RCC_CFGR |= 0x00020000;
-	RCC_CFGR &= ~0x00380000;
+	RCC_CFGR |= RCC_PLLMUL_X3_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X3_2;
 	#endif
 	#ifdef RCC_PLL_MUL_4
-	RCC_CFGR |= 0x00080000;
-	RCC_CFGR &= ~0x00340000;
+	RCC_CFGR |= RCC_PLLMUL_X4_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X4_2;
 	#endif
 	#ifdef RCC_PLL_MUL_5
-	RCC_CFGR |= 0x000C0000;
-	RCC_CFGR &= ~0x00300000;
+	RCC_CFGR |= RCC_PLLMUL_X5_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X5_2;
 	#endif
 	#ifdef RCC_PLL_MUL_6
-	RCC_CFGR |= 0x00100000;
-	RCC_CFGR &= ~0x002C0000;
+	RCC_CFGR |= RCC_PLLMUL_X6_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X6_2;
 	#endif
 	#ifdef RCC_PLL_MUL_7
-	RCC_CFGR |= 0x00140000;
-	RCC_CFGR &= ~0x00280000;
+	RCC_CFGR |= RCC_PLLMUL_X7_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X7_2;
 	#endif
 	#ifdef RCC_PLL_MUL_8
-	RCC_CFGR |= 0x00180000;
-	RCC_CFGR &= ~0x00240000;
+	RCC_CFGR |= RCC_PLLMUL_X8_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X8_2;
 	#endif
 	#ifdef RCC_PLL_MUL_9
-	RCC_CFGR |= 0x001C0000;
-	RCC_CFGR &= ~0x00200000;
+	RCC_CFGR |= RCC_PLLMUL_X9_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X9_2;
 	#endif
 	#ifdef RCC_PLL_MUL_10
-	RCC_CFGR |= 0x00200000;
-	RCC_CFGR &= ~0x001C0000;
+	RCC_CFGR |= RCC_PLLMUL_X10_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X10_2;
 	#endif
 	#ifdef RCC_PLL_MUL_11
-	RCC_CFGR |= 0x00240000;
-	RCC_CFGR &= ~0x00180000;
+	RCC_CFGR |= RCC_PLLMUL_X11_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X11_2;
 	#endif
 	#ifdef RCC_PLL_MUL_12
-	RCC_CFGR |= 0x00280000;
-	RCC_CFGR &= ~0x00140000;
+	RCC_CFGR |= RCC_PLLMUL_X12_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X12_2;
 	#endif
 	#ifdef RCC_PLL_MUL_13
-	RCC_CFGR |= 0x002C0000;
-	RCC_CFGR &= ~0x00100000;
+	RCC_CFGR |= RCC_PLLMUL_X13_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X13_2;
 	#endif
 	#ifdef RCC_PLL_MUL_14
-	RCC_CFGR |= 0x00300000;
-	RCC_CFGR &= ~0x000C0000;
+	RCC_CFGR |= RCC_PLLMUL_X14_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X14_2;
 	#endif
 	#ifdef RCC_PLL_MUL_15
-	RCC_CFGR |= 0x00340000;
-	RCC_CFGR &= ~0x00080000;
+	RCC_CFGR |= RCC_PLLMUL_X15_1;
+	RCC_CFGR &= ~RCC_PLLMUL_X15_2;
 	#endif
 	#ifdef RCC_PLL_MUL_16
-	RCC_CFGR |= 0x003C0000;
+	RCC_CFGR |= RCC_PLLMUL_X16;
 	#endif
 	// Enable the PLL clock source
-	RCC_CR |= (1<<24);
-	while ((RCC_CR & (1<<25)) == 0);
+	RCC_CR |= RCC_CR_PLL_ON;
+	while ((RCC_CR & RCC_CR_PLL_ON_RDY) == 0);
 	// select PLL as the system clock
-	RCC_CFGR |= (1<<1);
-	RCC_CFGR &= ~(1<<0);
-	while ((RCC_CFGR & 0x00000002) != 2);
+	RCC_CFGR |= RCC_CFGR_SW_PLL1;
+	RCC_CFGR &= ~RCC_CFGR_SW_PLL2;
+	// wait for the SWS flag to be set
+	while ((RCC_CFGR & RCC_CFGR_SWS_PLL) != 8);
 	#else
-		#error ("Wrong clock source choice!")
+		#error ("Wrong clock source choice!") //use #error only with preprocessor operations
 	#endif
 
 	// enable the clock security system (it's disabled by default)
 	#ifdef RCC_CSS
-	RCC_CR |= (1<<19);
+	RCC_CR |= RCC_CR_CSS_ON;
 	#endif
 
 	// set the prescaler of AHB, APB1, and APB2 bus whether the system clock is not divided
-	// or divided by 2 or 4 or 8 or 16
+	// or divided by 2, 4, 8, or 16
 	#ifdef RCC_APB2_NO_PRESCALER
-	RCC_CFGR &= ~0x00002000;
+	RCC_CFGR &= ~RCC_CFGR_PPRE2_DIV_1;
 	#endif
 	#ifdef RCC_APB2_PRESCALER_2
-	RCC_CFGR |= 0x00002000;
-	RCC_CFGR &= ~0x00001800;
+	RCC_CFGR |= RCC_CFGR_PPRE2_DIV_2_1;
+	RCC_CFGR &= ~RCC_CFGR_PPRE2_DIV_2_2;
 	#endif
 	#ifdef RCC_APB2_PRESCALER_4
-	RCC_CFGR |= 0x00002800;
-	RCC_CFGR &= ~0x00001000;
+	RCC_CFGR |= RCC_CFGR_PPRE2_DIV_4_1;
+	RCC_CFGR &= ~RCC_CFGR_PPRE2_DIV_4_2;
 	#endif
 	#ifdef RCC_APB2_PRESCALER_8
-	RCC_CFGR |= 0x00003000;
-	RCC_CFGR &= ~0x00000800;
+	RCC_CFGR |= RCC_CFGR_PPRE2_DIV_8_1;
+	RCC_CFGR &= ~RCC_CFGR_PPRE2_DIV_8_2;
 	#endif
 	#ifdef RCC_APB2_PRESCALER_16
-	RCC_CFGR |= 0x00003800;
+	RCC_CFGR |= RCC_CFGR_PPRE2_DIV_16;
 	#endif
 
 	#ifdef RCC_APB1_NO_PRESCALER
-	RCC_CFGR &= ~0x00004000;
+	RCC_CFGR &= ~RCC_CFGR_PPRE1_DIV_1;
 	#endif
 	#ifdef RCC_APB1_PRESCALER_2
-	RCC_CFGR |= 0x00004000;
-	RCC_CFGR &= ~0x00003000;
+	RCC_CFGR |= RCC_CFGR_PPRE1_DIV_2_1;
+	RCC_CFGR &= ~RCC_CFGR_PPRE1_DIV_2_2;
 	#endif
 	#ifdef RCC_APB1_PRESCALER_4
-	RCC_CFGR |= 0x00005000;
-	RCC_CFGR &= ~0x00002000;
+	RCC_CFGR |= RCC_CFGR_PPRE1_DIV_4_1;
+	RCC_CFGR &= ~RCC_CFGR_PPRE1_DIV_4_1;
 	#endif
 	#ifdef RCC_APB1_PRESCALER_8
-	RCC_CFGR |= 0x00006000;
-	RCC_CFGR &= ~0x00001000;
+	RCC_CFGR |= RCC_CFGR_PPRE1_DIV_8_1;
+	RCC_CFGR &= ~RCC_CFGR_PPRE1_DIV_8_1;
 	#endif
 	#ifdef RCC_APB1_PRESCALER_16
-	RCC_CFGR |= 0x00007000;
+	RCC_CFGR |= RCC_CFGR_PPRE1_DIV_16;
 	#endif
 
 	#ifdef RCC_AHB_NO_PRESCALER
-	RCC_CFGR &= ~0x00000080;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_1;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_2
-	RCC_CFGR |= 0x00000080;
-	RCC_CFGR &= ~0x00000070;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_2_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_2_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_4
-	RCC_CFGR |= 0x00000090;
-	RCC_CFGR &= ~0x00000060;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_4_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_4_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_8
-	RCC_CFGR |= 0x000000A0;
-	RCC_CFGR &= ~0x00000050;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_8_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_8_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_16
-	RCC_CFGR |= 0x000000B0;
-	RCC_CFGR &= ~0x00000040;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_16_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_16_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_64
-	RCC_CFGR |= 0x000000C0;
-	RCC_CFGR &= ~0x00000030;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_64_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_64_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_128
-	RCC_CFGR |= 0x000000D0;
-	RCC_CFGR &= ~0x00000020;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_128_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_128_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_256
-	RCC_CFGR |= 0x000000E0;
-	RCC_CFGR &= ~0x00000010;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_256_1;
+	RCC_CFGR &= ~RCC_CFGR_HPRE_DIV_256_2;
 	#endif
 	#ifdef RCC_AHB_PRESCALER_512
-	RCC_CFGR |= 0x000000F0;
+	RCC_CFGR |= RCC_CFGR_HPRE_DIV_512;
 	#endif
 }
 
